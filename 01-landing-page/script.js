efeeb6033582332c4f113ed95e748e8cfdd70d31
IT MANAGER <>
2023-06-03 14:37:12 +0530

    feat: added scramble text effect

--- 01-landing-page/script.js
+++ 01-landing-page/script.js
@@ -2,98 +2,138 @@
 const { round, random, floor } = Math;
 const $ = (selector) => document.querySelector(selector);
 const $all = (selector) => document.querySelectorAll(selector);
-const Emotes = [
-  `^_~`,
-  `-_+`,
-  `U_U`,
-  `O.O`,
-  `O_<`,
-  `~_~`,
-  `X_X`,
-  `O_O`,
-  `T_T`,
-  `¬_¬`,
-  `+_+`,
-  `>_<`,
-  `-_-`,
-];
-const Colors = [];
+const replaceCharAt = (str, char, i) =>
+  str.substring(0, i) + char + str.substring(i + 1);
+const randInt = (max, min = 0) => floor(random() * (max - min)) + min;
+const randItem = (arr) => arr[randInt(arr.length)];
 
-function randItem(arr) {
-  const randomIndex = floor(random() * arr.length);
-  return arr[randomIndex];
+const Emotes =
+  `^_~ -_+ U_U O.O O_< ~_~ X_X O_O T_T ¬_¬ +_+ >_< -_- ಠ_ಠ ʕ•ᴥ•ʔ =＾● ⋏ ●＾= ~(˘▾˘~) o_O O.o *-* //_^ ~,~ [¬º-°]¬`.split(
+    " "
+  );
+const LoremWords =
+  "lorem ipsum dolor consectetur adipiscing Nullam viverra turpis facilisis iaculis Curabitur felis tempor euismod pretium imperdiet Pellentesque varius hendrerit vitae porttitor Lorem Morbi porta Suspendisse accumsan bibendum laoreet eleifend Aliquam cursus magna augue elementum Proin scelerisque tincidunt rhoncus auctor Donec nulla ultricies faucibus feugiat risus finibus tortor".split(
+    " "
+  );
+
+const scrambleText =
+  `0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-=[]\;',./~!@#$%^&*()_+{}|:"<>?`.split(
+    ""
+  );
+// console.log(scrambleText);
+// debugger;
+function scramble(HTMLElement, text, times = randInt(15)) {
+  setTimeout(() => (HTMLElement.innerText = text), (times + 1) * 100);
+
+  for (times; times > 0; times--) {
+    setTimeout(() => {
+      let newText = "";
+      for (let i = 0; i < text.length; i++) {
+        newText += text.charAt(i) === " " ? " " : randItem(scrambleText);
+      }
+      // console.log(text);
+      // console.log(newText);
+      HTMLElement.innerText = newText;
+    }, times * 100);
+  }
+}
+function isInViewport(element) {
+  // Get the bounding client rectangle position in the viewport
+  const bounding = element.getBoundingClientRect();
+
+  // Checking part. Here the code checks if it's *fully* visible
+  // Edit this part if you just want a partial visibility
+  if (
+    bounding.top >= 0 &&
+    bounding.left >= 0 &&
+    bounding.right <=
+      (window.innerWidth || document.documentElement.clientWidth) &&
+    bounding.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight)
+  ) {
+    // console.log("In the viewport! :)");
+    return true;
+  } else {
+    // console.log("Not in the viewport. :(");
+    return false;
+  }
 }
 
 const update = {
-  emote: () => {
-    for (let i = 0; i < 5; i++)
-      setTimeout(
-        () => $('#emote').innerText = randItem(Emotes),
-        100 * i);
-  },
-  lorem: () => {
-    word = ""
-    do { word = pickWord() }
-    while (word.length <= 5);
-    $('#lorem').innerText = word
-  },
+  emote: () => scramble($("#emote"), randItem(Emotes), 5),
+  lorem: () => scramble($("#lorem"), randItem(LoremWords), 10),
   compliment: () => {
     fetch("https://complimentr.com/api")
-      .then(response => response.json())
-      .then(({ compliment }) => $('#compliment').innerText = compliment)
-  }
+      .then((response) => response.json())
+      .then(({ compliment }) => scramble($("#compliment"), compliment));
+  },
+  quote: () => {
+    fetch("https://api.quotable.io/quotes/random?minLength=50")
+      .then((response) => response.json())
+      .then((object) => object[0])
+      .then(({ content }) => scramble($("#quote"), content, 30));
+  },
 };
 
 function Init() {
   update.emote();
-  update.lorem();
+  update.quote();
 
-
-  // $all(".page").forEach((page) => {
-  //   console.log(window.innerWidth)
-  //   page.style.width = window.innerWidth + "px"
-  //   page.style.height = window.innerHeight + "px"
-  // })
-  update.compliment();
-
-  window.onmousemove = cursor
-  // window.onblur = () => $("#cursor").style.opacity = 0
+  window.onmousemove = cursor;
+  window.onscroll = scrollObserver;
 }
 Init();
 
 function cursor(e) {
-  const cursor = $("#cursor")
-  const target = e.target
-  let newWidth = "1rem"
+  const cursor = $("#cursor");
+  const target = e.target;
+  let newWidth = "1rem";
   if (target.classList.contains("hover")) {
-    const { height } = target.getBoundingClientRect()
+    const { height } = target.getBoundingClientRect();
     // console.log(height)
-    newWidth = height + "px"
+    newWidth = height + "px";
   }
 
   if (target.classList.contains("clickable")) {
-    cursor.classList.add("clickable")
+    cursor.classList.add("clickable");
+  } else {
+    cursor.classList.remove("clickable");
   }
-  else
-    cursor.classList.remove("clickable")
 
-  cursor.style.width = newWidth
+  cursor.style.width = newWidth;
 
-  let cursorWidth = 0
+  let cursorWidth = 0;
+
   if (newWidth.includes("rem"))
-    cursorWidth = parseInt(newWidth.replace("rem", "")) * 16
+    cursorWidth = parseInt(newWidth.replace("rem", "")) * 16;
   else if (newWidth.includes("px"))
-    cursorWidth = parseInt(newWidth.replace("px", ""))
+    cursorWidth = parseInt(newWidth.replace("px", ""));
 
-  let x = (e.clientX - cursorWidth / 2).toFixed(0, 0)
-  let y = (e.clientY - cursorWidth / 2).toFixed(0, 0)
+  // parseInt(newWidth.includes("rem")?.replace("rem","")) * 16
+  // .replace("px", "")
+
+  // let x = (e.clientX - cursorWidth / 2).toFixed(0, 0);
+  // let y = (e.clientY - cursorWidth / 2).toFixed(0, 0);
   // console.log(x, y)
-  cursor.style.opacity = "1"
-  cursor.style.left = x + "px"
-  cursor.style.top = y + "px"
+  cursor.style.opacity = "1";
+  cursor.style.left = (e.clientX - cursorWidth / 2).toFixed(0, 0) + "px";
+  cursor.style.top = (e.clientY - cursorWidth / 2).toFixed(0, 0) + "px";
 
   // cursor.style.width = (e.target.classList.contains("hover")) ? "3rem" : "1rem"
   // if(e.target.class)
+}
+
+var animated = false;
+function scrollObserver() {
+  if (
+    isInViewport($("#lorem")) &&
+    isInViewport($("#compliment")) &&
+    !animated
+  ) {
+    update.lorem();
+    update.compliment();
+    animated = true;
+  }
 }
 
 // + " ↻"
